06c1e7d85f1884c45585ff1251ac97a09d4cf29d
AL03R14-2 <erika-1930@live.com.mx>
2015-08-18 11:46:33 -0500

    tres-2

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,24 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
-	
-}); 
+//document.addEventListener("deviceready",function(){
+$('#btn_sonido').click(function(){
+   if($('#btn_sonido').hasClass('ui-icon-audio'))
+   {
+	   //apagar el audio
+	   $('#btn_sonido').removeClass('ui-icon-audio');
+	   $('#btn_sonido').addClass('ui-icon-forbidden');
+	   alert('se apago el sonido');
+   }
+   else
+   {
+	   //prender sonido
+	$('#btn_sonido').removeClass('ui-icon-forbidden');
+	   $('#btn_sonido').addClass('ui-icon-audio');
+	   alert('Se prendio el sonido');   
+	   navigator.notification.beep(1);
+   }
+    });	//click a btn_sonido
+
+//}); 
 });
 
